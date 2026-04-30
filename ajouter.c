9b79105cce72c84a02159dfe7689c7dbf7e3c678
manuely AKAKPO-NUMADO <akanueli11@gmail.com>
2026-04-30 00:15:32 +0000

    Add core structure for student computer management app
    Adds initial implementation of a console-based application for managing student computers, including modular source files for various operations.

--- ajouter.c
+++ ajouter.c
@@ -1,3 +1,27 @@
+/*
+Nom du programme : Deliberation_UE
+
+GROUPE 4
+
+Auteurs:
+
+AKAKPO-NUMADO manuely IA&BD
+NAM Moussah Yedouboime  GC
+NIKABOU Kondi Chérif   GC
+SEDALO Kokou Egnon Nathalien  GC
+ABALO Kokou Mawussé Godfroy GC
+ADEGUE Komi Emile GM
+SAPKANI Paguiyedou IA&BD
+LAMANALA Di-bahome Timothée GM
+KADJOU Sokoum Paul  GC
+
+Date : 21 Avril 2026
+
+Description : Adds core structure for student computer management app
+
+Introduces initial implementation of a console-based application to manage student computers, including modular source files for adding, displaying, modifying, deleting, searching, sorting, and navigating computer records. 
+*/
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
